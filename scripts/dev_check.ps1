78631f7ac383154759226032e59ed1cfb2a50707
aubronsamuel-hash <aubronsamuel@gmail.com>
2025-08-18 03:44:17 +0400

    Delete scripts directory

--- scripts/dev_check.ps1
+++
@@ -1,6 +0,0 @@
-$ErrorActionPreference = "Stop"
-if (Test-Path api) { ruff check api; mypy api; pytest -q api }
-if (Test-Path front) { npm --prefix front ci; npm --prefix front run build }
-if (Test-Path api/Dockerfile) { docker build -t test-api ./api }
-if (Test-Path front/Dockerfile) { docker build -t test-front ./front }
-Write-Output "dev_check OK"
